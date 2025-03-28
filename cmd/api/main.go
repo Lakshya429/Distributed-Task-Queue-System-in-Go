3578e2a086e96827175a0576b0d69b7731ef899e
Lakshya429 <lakshyathakur429@gmail.com>
2025-03-28 23:02:57 +0530

    database 9

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/Lakshya429/distributed-task-queue/internal/api"
+	"github.com/Lakshya429/distributed-task-queue/pkg/database"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	database.InitDB()
 	api.RoutesHandles(router)
 	router.Run()
 }
